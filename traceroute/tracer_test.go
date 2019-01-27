db3d0ded0597db5256386775222d2d851886f4bb
pixelbender <pixelbender@yandex.ru>
2019-01-28 02:19:11 +0300

    Add examples

--- traceroute/tracer_test.go
+++ traceroute/tracer_test.go
@@ -1,14 +1,17 @@
-package traceroute
+package traceroute_test
 
 import (
 	"context"
+	"github.com/pixelbender/go-traceroute/traceroute"
+	"log"
 	"net"
 	"testing"
+	"time"
 )
 
 func TestTraceReply(t *testing.T) {
 	ip := net.ParseIP("8.8.8.8")
-	err := DefaultTracer.Trace(context.Background(), ip, func(reply *Reply) {
+	err := traceroute.DefaultTracer.Trace(context.Background(), ip, func(reply *traceroute.Reply) {
 		t.Logf("%d. %v %v", reply.Hops, reply.IP, reply.RTT)
 	})
 	if err != nil {
@@ -18,7 +21,7 @@ func TestTraceReply(t *testing.T) {
 
 func TestTrace(t *testing.T) {
 	ip := net.ParseIP("8.8.8.8")
-	hops, err := Trace(ip)
+	hops, err := traceroute.Trace(ip)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -26,5 +29,36 @@ func TestTrace(t *testing.T) {
 		for _, n := range h.Nodes {
 			t.Logf("%d. %v %v", h.Distance, n.IP, n.RTT)
 		}
+	}
+}
+
+func ExampleSimple() {
+	hops, err := traceroute.Trace(net.ParseIP("8.8.8.8"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, h := range hops {
+		for _, n := range h.Nodes {
+			log.Printf("%d. %v %v", h.Distance, n.IP, n.RTT)
+		}
+	}
+}
+
+func ExampleCustom() {
+	t := &traceroute.Tracer{
+		Config: traceroute.Config{
+			Delay:   50 * time.Millisecond,
+			Timeout: time.Second,
+			MaxHops: 30,
+			Count:   3,
+			Network: "ip4:ip",
+		},
+	}
+	defer t.Close()
+	err := t.Trace(context.Background(), net.ParseIP("8.8.8.8"), func(reply *traceroute.Reply) {
+		log.Printf("%d. %v %v", reply.Hops, reply.IP, reply.RTT)
+	})
+	if err != nil {
+		log.Fatal(err)
 	}
 }
