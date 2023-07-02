a4d4dedf95e31aa6c5c8ebf138e40c2cc4282658
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-07-02 11:06:47 +0200

    multiple

--- main.go
+++ main.go
@@ -15,24 +15,33 @@ func main() {
 			Delay:    50 * time.Millisecond,
 			Timeout:  time.Second,
 			MaxHops:  30,
-			Count:    1,
+			Count:    3,
 			Networks: []string{"ip4:icmp", "ip4:ip"},
 		},
 	}
 	defer t.Close()
-	replies := make(map[int]*traceroute.Reply)
-	hops := []int{}
+	replies := make(map[int][]*traceroute.Reply)
 	err := t.Trace(context.Background(), net.ParseIP("1.1.1.1"), func(reply *traceroute.Reply) {
 		//log.Printf("%d. %v %v", reply.Hops, reply.IP, reply.RTT)
-		replies[reply.Hops] = reply
-		hops = append(hops, reply.Hops)
+		replies[reply.Hops] = append(replies[reply.Hops], reply)
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	printHops(replies)
+}
+
+func printHops(replies map[int][]*traceroute.Reply) {
+	hops := []int{}
+	for hop := range replies {
+		hops = append(hops, hop)
+	}
 	sort.Ints(hops)
 	for _, hop := range hops {
-		reply := replies[hop]
-		log.Printf("%d. %v %v", reply.Hops, reply.IP, reply.RTT)
+		replyList := replies[hop]
+		for _, reply := range replyList {
+			log.Printf("%d. %v %v", reply.Hops, reply.IP, reply.RTT)
+		}
 	}
 }
